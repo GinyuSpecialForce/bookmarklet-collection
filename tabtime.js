abcc09642a79a5cafd47762433b3577d239b5e9e
GinyuSpecialForce <shenronrs3@gmail.com>
2024-02-11 15:52:18 -0800

    Create tabtime.js

---
+++ tabtime.js
@@ -0,0 +1,1 @@
+javascript:(function(d,w,f,t,e){t=(e=w.elp$||0)?clearInterval(e,d.title=odT,elp$=0)&&0:f(w.elp$=setInterval(function(){d.title=(new%20Date(f()-t)).toISOString().substr(11,8)},40),w.odT=d.title)})(document,window,function(){return(new%20Date())|0});
