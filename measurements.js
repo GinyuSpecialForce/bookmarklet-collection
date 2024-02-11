fd769472ddb9e462d73e4844bb9f2157a65f3bca
GinyuSpecialForce <shenronrs3@gmail.com>
2024-02-11 15:51:30 -0800

    Create measurements.js

---
+++ measurements.js
@@ -0,0 +1,1 @@
+javascript:function fnStartDesign(sUrl) {var nScript = document.createElement('script');nScript.setAttribute('language','JavaScript');nScript.setAttribute('src',sUrl);document.body.appendChild(nScript);}fnStartDesign('https://www.sprymedia.co.uk/design/design/media/js/design-loader.js');
